d885c2ce12e3e00282230fb9adcefb1ead1d66b6
Jose Hernandez <josemiguelhernandezsousa@gmail.com>
2024-12-22 10:27:12 -0400

    Fixed minor issues

--- src/front/App.jsx
+++ src/front/App.jsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Home from './Home.jsx'
-import Navbar from './components/navbar.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Home from "./Home.jsx";
+import Navbar from "./components/Navbar.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Navbar />
     <Home />
-  </StrictMode>,
-)
+  </StrictMode>
+);
--- src/front/components/Navbar.jsx
+++ src/front/components/Navbar.jsx
@@ -1,18 +1,16 @@
 import React from "react";
-import Logo from "../img/logo.jsx";
+import Logo from "../img/Logo.jsx";
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between px-4 mb-4 items-center text-primary">
+    <div className='flex justify-between px-4 mb-4 items-center text-primary'>
       <Logo />
-      <div className="flex gap-4">
-        <h3 className="px-4">Profile</h3>
-        <h3 className="px-4">Settings</h3>
-        <h3 className="px-4">About</h3>
-        <h3 className="px-4">Contact</h3>
-        <h3 className="border-2 rounded px-4">
-          Login
-        </h3>
+      <div className='flex gap-4'>
+        <h3 className='px-4'>Profile</h3>
+        <h3 className='px-4'>Settings</h3>
+        <h3 className='px-4'>About</h3>
+        <h3 className='px-4'>Contact</h3>
+        <h3 className='border-2 rounded px-4'>Login</h3>
       </div>
     </div>
   );
